268fa0f671cafbc7ca4e1e5cc1737c56b8b792bb
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-07 13:53:58 +0200

    feat(component): add NavLink from  react-router for ButtonLink component

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,8 +1,9 @@
+import { NavLink } from "react-router";
 import styles from "../assets/styles/components/buttonLink.module.scss";
 
-export default function ButtonLink({ id, text }) {
+export default function ButtonLink({ path, text }) {
 
     return (
-        <button className={ styles.buttonLink } id={ id }>{ text }</button>
+        <NavLink className={ ({ isActive }) => isActive ? `${styles.buttonLink} ${styles.buttonLink__active}` : styles.buttonLink } to={ path }>{ text }</NavLink>
     );
 };
--- src/views/NavHeaderView.jsx
+++ src/views/NavHeaderView.jsx
@@ -2,11 +2,11 @@ import styles from "../assets/styles/views/navHeaderView.module.scss";
 import ButtonLink from '../components/ButtonLink.jsx';
 
 export default function NavHeaderView() {
-    const routes = ["See Tasks", "About", "My follow"];
-    
+    const routes = ["tasks", "about", "follow"];
+
     return (
         <nav className={ styles.navHeaderView }>
-            { routes.map((itt, index) => <ButtonLink key={ index } id={ index } text={ itt }/>) }
+            { routes.map((itt, index) => <ButtonLink key={ index } path={ itt } text={ itt }/>) }
         </nav>
     );
 };
