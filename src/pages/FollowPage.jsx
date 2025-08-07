3597580f9f20ce4b187c29b2959d18002c20d151
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-07 11:57:03 +0200

    chore(rename): rename MyFollowPage to FollowPage

---
+++ src/pages/FollowPage.jsx
@@ -0,0 +1,8 @@
+import styles from "../assets/styles/pages/followPage.module.scss";
+
+export default function FollowPage({ children }) {
+
+    return (
+        <div className={ styles.followPage }>{ children }</div>
+    );
+};
--- src/pages/MyFollowPage.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "../assets/styles/pages/myFollowPage.module.scss";
-
-export default function MyFollowPage({ children }) {
-
-    return (
-        <div className={ styles.myFollowPage }>{ children }</div>
-    );
-};
