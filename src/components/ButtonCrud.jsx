fc67c6a63c41a3f8044a49916f52538d3323618e
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-09 15:35:20 +0200

    style(component): update ButtonCrud styles

--- src/components/ButtonCrud.jsx
+++ src/components/ButtonCrud.jsx
@@ -3,7 +3,8 @@ import styles from "../assets/styles/components/buttonCrud.module.scss";
 export default function ButtonCrud({ option }) {
 
     return (
-        <li className={ styles.buttonCrud }>    
+        <li className={ styles.buttonCrud }>   
+            <span>puce</span> 
             <button>{ option }</button>
         </li>
     );
