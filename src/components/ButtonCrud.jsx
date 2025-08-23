d5f738d4e7f1373e64932fbc9a96506344da4bc9
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-23 12:31:55 +0200

    style(component): apply new styles of ButtonCrud component

--- src/components/ButtonCrud.jsx
+++ src/components/ButtonCrud.jsx
@@ -1,12 +1,14 @@
 import styles from "../assets/styles/components/buttonCrud.module.scss";
 
-export default function ButtonCrud({ img, option, handleClick }) {
+export default function ButtonCrud({ option, children, handleClick }) {
 
     return (
-        <button className={ styles.buttonCrud } onClick={ handleClick }>   
-            <span>puce</span> 
-            <img src={ img } alt={`${ option } task !`} />
-            { option }
-        </button>
+        <div className={ styles.buttonCrud }>
+            <button onClick={ handleClick }>   
+                <span>puce</span> 
+                { option }
+            </button>
+            { children }
+        </div>
     );
 };
--- src/views/OptionsCrudView.jsx
+++ src/views/OptionsCrudView.jsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
 import styles from "../assets/styles/views/optionsCrudView.module.scss";
-import btnSave from "../assets/images/btnSave.svg";
-import btnBack from "../assets/images/btnBack.svg";
-import btnCheck from "../assets/images/btnCheck.svg";
-import btnEdit from "../assets/images/btnEdit.svg";
-import btnBin from "../assets/images/btnBin.svg";
 import ButtonClose from "../components/ButtonClose.jsx";
 import ButtonCrud from "../components/ButtonCrud.jsx";
 import ButtonKbd from "../components/ButtonKbd.jsx";
@@ -27,14 +22,24 @@ export default function OptionsCrudView({ id, isOpen, handleToggle, doneTask, ed
                     updateSelect === true
                         ? (        
                             <>
-                                <ButtonCrud handleClick={ () => doneTask(id) } img={ btnSave } option="save" />
-                                <ButtonCrud handleClick={ () => editTask(id) } img={ btnBack } option="cancel" />
+                                <ButtonCrud handleClick={ () => doneTask(id) } option="save">
+                                    <ButtonKbd text="S" styleMode="white"/>
+                                </ButtonCrud>
+                                <ButtonCrud handleClick={ () => editTask(id) } option="cancel">
+                                    <ButtonKbd text="C" styleMode="white"/>
+                                </ButtonCrud>
                             </>
                         ) : (
                             <>
-                                <ButtonCrud handleClick={ () => doneTask(id) } img={ btnCheck } option="validate" />
-                                <ButtonCrud handleClick={ () => { editTask(id); handleToggleUpdateSelect() }} img={ btnEdit } option="update" />
-                                <ButtonCrud handleClick={ () => deleteTask(id) } img={ btnBin } option="delete" /> 
+                                <ButtonCrud handleClick={ () => doneTask(id) } option="validate">
+                                    <ButtonKbd text="V" styleMode="white"/>
+                                </ButtonCrud>
+                                <ButtonCrud handleClick={ () => { editTask(id); handleToggleUpdateSelect() }} option="update">
+                                    <ButtonKbd text="U" styleMode="white"/>
+                                </ButtonCrud>
+                                <ButtonCrud handleClick={ () => deleteTask(id) } option="delete">
+                                    <ButtonKbd text="D" styleMode="white"/>
+                                </ButtonCrud> 
                             </>
                         )
                 }
